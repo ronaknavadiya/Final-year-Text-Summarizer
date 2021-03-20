72edf5b5a3e1c63cc867c388b8357896a251017d
ronaknavadiya <ronaknavadiya123@gmail.com>
2021-03-20 14:48:50 +0530

    finish upto main_summary

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from gensim.summarization import summarize
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from flask import Flask, render_template, url_for, request
+from main_summary import main_summary
 
 from spacy_summarization import text_summarizer
 import time
@@ -65,15 +66,14 @@ def index():
 
 @app.route('/analyze', methods=['GET', 'POST'])
 def analyze():
-    start = time.time()
     if request.method == 'POST':
         rawtext = request.form['rawtext']
         final_reading_time = readingTime(rawtext)
-        final_summary = text_summarizer(rawtext)
+        final_summary = main_summary(rawtext)
         summary_reading_time = readingTime(final_summary)
-        end = time.time()
-        # final_time = end-start
-    return render_template('index.html', ctext=rawtext, final_summary=final_summary, final_reading_time=final_reading_time, summary_reading_time=summary_reading_time)
+        spacy_words = len(final_summary.split())
+        ctext_words = len(rawtext.split())
+    return render_template('index.html', ctext_words=ctext_words, spacy_words=spacy_words, ctext=rawtext, final_summary=final_summary, final_reading_time=final_reading_time, summary_reading_time=summary_reading_time)
 
 
 @app.route('/compare')
@@ -83,20 +83,19 @@ def compare():
 
 @app.route('/analyze_url', methods=['GET', 'POST'])
 def analyze_url():
-    start = time.time()
     if request.method == 'POST':
         try:
             raw_url = request.form['raw_url']
         except urlopen.error.URLError as e:
-            ResponseData = e.read().decode("utf8", 'ignore')
-            print("error:", ResponseData)
+            data = e.read().decode("utf8", 'ignore')
+            print("error:", data)
         rawtext = get_text(raw_url)
         final_reading_time = readingTime(rawtext)
         final_summary = summarize(rawtext)
         summary_reading_time = readingTime(final_summary)
-        end = time.time()
-        final_time = end-start
-    return render_template('index.html', final_summary=final_summary, summary_reading_time=summary_reading_time, ctext=rawtext, final_reading_time=final_reading_time,)
+        spacy_words = len(final_summary.split())
+        ctext_words = len(rawtext.split())
+    return render_template('index.html', ctext_words=ctext_words, spacy_words=spacy_words, final_summary=final_summary, summary_reading_time=summary_reading_time, ctext=rawtext, final_reading_time=final_reading_time,)
 
 
 @app.route('/analyze_compare_url', methods=['GET', 'POST'])
@@ -104,7 +103,6 @@ def analyze_compare_url():
     raw_url = request.form['raw_url']
     rawtext = get_text(raw_url)
     # Spacy Summarizer
-    final_reading_time = readingTime(rawtext)
     final_summary_spacy = text_summarizer(rawtext)
     summary_reading_time = readingTime(final_summary_spacy)
     spacy_words = len(final_summary_spacy.split())
@@ -126,10 +124,10 @@ def analyze_compare_url():
 
 @app.route('/comparer', methods=['GET', 'POST'])
 def comparer():
-    start = time.time()
+
     if request.method == 'POST':
         rawtext = request.form['rawtext']
-        final_reading_time = readingTime(rawtext)
+        # Spacy
         final_summary_spacy = text_summarizer(rawtext)
         summary_reading_time = readingTime(final_summary_spacy)
         spacy_words = len(final_summary_spacy.split())
@@ -146,8 +144,6 @@ def comparer():
         summary_reading_time_sumy = readingTime(final_summary_sumy)
         sumy_words = len(final_summary_sumy.split())
 
-        end = time.time()
-        final_time = end-start
     return render_template('compare.html', spacy_words=spacy_words, genism_words=genism_words, nltk_words=nltk_words, sumy_words=sumy_words, final_summary_spacy=final_summary_spacy, final_summary_gensim=final_summary_gensim, final_summary_nltk=final_summary_nltk,  final_summary_sumy=final_summary_sumy, summary_reading_time=summary_reading_time, summary_reading_time_gensim=summary_reading_time_gensim, summary_reading_time_sumy=summary_reading_time_sumy, summary_reading_time_nltk=summary_reading_time_nltk)
 
 
